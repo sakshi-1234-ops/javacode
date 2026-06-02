73888ff508b8c3acdd5442e10d8e5c244de35056
sakshi-1234-ops <sutarsakshi098@gmail.com>
2026-06-02 13:54:50 +0530

    Added java program

---
+++ p1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class p1{
+          public static void main(String args[])
+          {
+               Scanner sc = new Scanner(System.in);
+               int a,c;     
+               System.out.println("enter a number:");
+               a = sc.nextInt();
+               c = sc.nextInt();
+               c = a*a;
+               System.out.println("square="+c);
+           }
+}
